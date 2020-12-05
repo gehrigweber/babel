00704011fba39169999624661843b0f88f018dd5
Gehrig Weber <gehrigweber@umail.ucsb.edu>
2020-12-04 17:23:54 -0800

    GW: Program improved; more voices added
    Program improvements:
    Uses random.shuffle instead of randint, sounds a lot nicer.
    Now gives the option to create multiple clips, instead of exiting after creating one clip.

--- main.py
+++ main.py
@@ -4,35 +4,46 @@ import uuid
 import pyphen
 from pydub import AudioSegment
 
+more = "yes"
+while more == "yes" or more == "Yes" or more == "y" or more == "Y":
+    char = input("Enter character name: ")
+    print("Will generate for", char)
 
-char = input("Enter character name: ")
-print("Will generate for", char)
-
-script = input("Enter script: ")
-
-
-filenames = os.listdir("./" + char)
-voice_clips = []
-for file in filenames:
-    voice_clips.append(AudioSegment.from_wav("./" + char + "/" + file))
-space = AudioSegment.from_wav("./common/space.wav")
-pause = AudioSegment.from_wav("./common/pause.wav")
+    script = input("Enter script: ")
 
 
-slicer = pyphen.Pyphen(lang = "nl_NL")
-syllables = slicer.inserted(script).replace(" -", " ")
-syllables = syllables.replace(", ", ",-")
-syllables = syllables.replace(". ", ".-")
-syllables = syllables.replace(" ", " -")
+    filenames = os.listdir("./voices/" + char)
+    voice_clips = []
+    for file in filenames:
+        voice_clips.append(AudioSegment.from_wav("./voices/" + char + "/" + file))
+    space = AudioSegment.from_wav("./common/space.wav")
+    pause = AudioSegment.from_wav("./common/pause.wav")
 
 
-result = voice_clips[random.randint(0, len(voice_clips) - 1)]
-for syllable in syllables:
-    if syllable == " ":
-        result = result + space
-    elif syllable in [",", ".", "?", "!"]:
-        result = result + pause
-    elif syllable == "-":
-        result = result + voice_clips[random.randint(0, len(voice_clips) - 1)]
+    slicer = pyphen.Pyphen(lang = "nl_NL")
+    syllables = slicer.inserted(script).replace(" -", " ")
+    syllables = syllables.replace(", ", ",-")
+    syllables = syllables.replace(". ", ".-")
+    syllables = syllables.replace(" ", " -")
 
-result.export("output/" + char + str(uuid.uuid4().hex) + ".wav", format="wav")
+
+    random.shuffle(voice_clips)
+    index = 0
+    result = voice_clips[index]
+    index = index + 1
+    for syllable in syllables:
+        if syllable == " ":
+            result = result + space
+        elif syllable in [",", ".", "?", "!"]:
+            result = result + pause
+        elif syllable == "-":
+            result = result + voice_clips[index]
+            index = index + 1
+            if index >= len(voice_clips):
+                random.shuffle(voice_clips)
+                index = 0
+
+    result.export("output/" + char + "-" + script + str(uuid.uuid4().hex) + ".wav", format="wav")
+
+    more = input("Keep going? ")
+    
